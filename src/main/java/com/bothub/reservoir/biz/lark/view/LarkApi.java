5d299e70f1cb0e2494c4aa6f4b1457ffb9c8dffe
guxingke <admin@guxingke.com>
2020-02-22 18:56:20 +0800

    :sparkles: text callback

--- src/main/java/com/bothub/reservoir/biz/lark/view/LarkApi.java
+++ src/main/java/com/bothub/reservoir/biz/lark/view/LarkApi.java
@@ -1,7 +1,11 @@
 package com.bothub.reservoir.biz.lark.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,15 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("lark")
+@Slf4j
 public class LarkApi {
 
+  private final ObjectMapper objectMapper;
+
+  @Autowired
+  public LarkApi(ObjectMapper objectMapper) {
+    this.objectMapper = objectMapper;
+  }
+
   @PostMapping("callback")
-  public Object callback(@RequestBody LarkCallback callback) {
+  public Object callback(@RequestBody LarkCallback callback) throws JsonProcessingException {
     if (callback.getType().equals("url_verification")) {
       Map<String, String> map = new HashMap<>();
       map.put("challenge", callback.getChallenge());
       return map;
     }
+    String rawJson = objectMapper.writeValueAsString(callback);
+    log.info(rawJson);
     return null;
   }
 }
--- src/main/java/com/bothub/reservoir/biz/lark/view/LarkCallback.java
+++ src/main/java/com/bothub/reservoir/biz/lark/view/LarkCallback.java
@@ -1,10 +1,35 @@
 package com.bothub.reservoir.biz.lark.view;
 
+import java.util.Date;
 import lombok.Data;
 
 @Data
 public class LarkCallback {
   private String challenge;
+  private String uuid;
   private String token;
+  private Date ts;
   private String type;
+  private Object event;
+//  {
+//    "uuid": "41b5f371157e3d5341b38b20396e77e3",
+//      "token": "2g7als3DgPW6Xp1xEpmcvgVhQG621bFY",//校验Token
+//      "ts": "1550038209.428520",  //时间戳
+//      "type": "event_callback",//事件回调此处固定为event_callback
+//      "event": {
+//    "type": "message", // 事件类型
+//        "app_id": "cli_xxx",
+//        "tenant_key": "xxx", //企业标识
+//        "root_id": "",
+//        "parent_id": "",
+//        "open_chat_id": "oc_5ce6d572455d361153b7cb51da133945",
+//        "chat_type": "private",//私聊private，群聊group
+//        "msg_type": "text",    //消息类型
+//        "open_id": "ou_18eac85d35a26f989317ad4f02e8bbbb",
+//        "open_message_id": "om_36686ee62209da697d8775375d0c8e88",
+//        "is_mention": false,
+//        "text": "消息内容",      // 消息文本
+//        "text_without_at_bot":"消息内容" //消息内容，会过滤掉at你的机器人的内容
+//  }
+//  }
 }
